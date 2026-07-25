410f137a764d14648efa164086bf8fe7d2ca214d
George Tackley <tackley@gmail.com>
2026-07-25 21:18:20 +0100

    ...

--- app.R
+++ app.R
@@ -737,8 +737,7 @@ server <- function(input, output) {
   }) %>% bindEvent(input$update)
   
   output$time_string <- renderText({
-    #paste("... Update completed at: ", as.character(floor_date(ymd_hms(Sys.time()))))
-    run_update()
+    paste("... Update completed at: ", as.character(floor_date(ymd_hms(Sys.time()))),run_update())
   })
 }
 
